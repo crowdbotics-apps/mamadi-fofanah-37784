c44b9efe5b6c38f5333e7034b58b596ca33c6da8
Crowdbotics <team@crowdbotics.com>
2022-12-09 21:15:26 +0000

    Update admin title

--- backend/yayajuju_37784/urls.py
+++ backend/yayajuju_37784/urls.py
@@ -35,9 +35,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "Mamadi Fofanah"
-admin.site.site_title = "Mamadi Fofanah Admin Portal"
-admin.site.index_title = "Mamadi Fofanah Admin"
+admin.site.site_header = "RewardsMaster"
+admin.site.site_title = "RewardsMaster Admin Portal"
+admin.site.index_title = "RewardsMaster Admin"
 
 # swagger
 api_info = openapi.Info(
